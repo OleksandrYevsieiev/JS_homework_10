de6efeb637ca2c859799abd87efb7875565f548e
oleksandryevsieiev <alexavenger@hotmail.com>
2020-10-14 16:42:01 +0300

    cards done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ListItem from './components/ListItem';
-import user from './users.json';
+import users from './users.json';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: users,
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <ListItem/>
-      <ListItem/>
-      <ListItem/>
-      <ListItem/>
-      <ListItem/>
-      <ListItem/>
-      <ListItem/>
-      <ListItem/>
-    </div>
-  );
+  makeKey = () => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < 50; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+
+  renderListItems = () => {
+    return this.state.userInfo.map((item) => (
+      <ListItem key={this.makeKey()} name={item} user={users} />
+    ));
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+      {this.renderListItems()}
+      </div>
+    )
+  }
 }
-
-export default App;
